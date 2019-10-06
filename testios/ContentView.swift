62c7151ccca20cd471efbaf97282770c0b0990d9
jackrex <jackrex1993@gmail.com>
2019-10-06 10:16:49 +0800

    Initial Commit

---
+++ testios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testios
+//
+//  Created by jackrex on 6/10/2019.
+//  Copyright © 2019 Sunny. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
